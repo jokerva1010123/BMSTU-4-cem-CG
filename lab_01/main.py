41db6e6d6d7426bcdd9b6b70a92401895fe6593d
Dinh Viet Anh <jokerva1010123@gmail.com>
2022-02-13 17:01:26 +0300

    Update main.py

--- lab_01/main.py
+++ lab_01/main.py
@@ -16,9 +16,19 @@ task_label = Label(window, text = '''Заданы два множества то
 В каждом множестве найти три равноудалённые друг от друга точки.
 Через эти точки провести окружности. В каждую окружность вписать шестиугольник.
 Найти площадь пересечения двух получивших шестиугольников.''', background="yellow")
-task_label.place(x = 10, y = 10)
-canvas = Canvas(window, width=800, height=700)
+task_label.place(x = 200, y = 10)
+list_box_label_1 = Label(window, text = "Множество точек 1", width = 20)
+list_box_label_1.place(x = 1200, y = 60)
+list_box_label_2 = Label(window, text = "Множество точек 2", width = 20)
+list_box_label_2.place(x = 1200, y = 400)
+
+canvas = Canvas(window, width=1000, height=700, bg = "white")
 canvas.place(x = 10, y = 100)
+
+list_box_1 = Listbox(window, selectmode = SINGLE, height = 15, width = 40)
+list_box_1.place(x = 1100, y = 100)
+list_box_2 = Listbox(window, selectmode = SINGLE, height = 15, width = 40)
+list_box_2.place(x = 1100, y = 430)
 
 info_button = Button(window, text = "?", command = show_info)
 info_button.place(x = 1350, y = 850)
